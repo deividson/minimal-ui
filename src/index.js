7abe7361c5f03eddf180aa6bc6c3cba99e15d360
Deividson M. Goulart <deividson.goulart@g.globo>
2020-10-18 23:03:07 -0300

    chore: module federation

--- src/index.js
+++ src/index.js
@@ -1,8 +1,3 @@
 export * from './components'
 export * from './style'
 export * from './utils'
-
-
-// import (/* webpackChunkName: "components" */ './components')
-// import (/* webpackChunkName: "style" */ './style')
-// import (/* webpackChunkName: "utils" */ './utils')
--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,6 @@ const webpack = require('webpack')
 const path = require('path')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const { ModuleFederationPlugin } = webpack.container
-// const HtmlWebpackPlugin = require('html-webpack-plugin')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 const TerserPlugin = require('terser-webpack-plugin')
 
@@ -12,18 +11,10 @@ const isProduction = env === 'production'
 const addOptions = {
   devtool: !isProduction ? 'inline-source-map' : undefined,
 }
-
-// TODO receber publicPath como argumento pra definir destino no deploy
-const publicPath = !isProduction ? 'http://localhost:3002/' : 'https://s3.glbimg.com/v1/AUTH_ba8e460c944543468c3c80cb9675751f/lab/minimal/demo/'
-
 module.exports = {
   mode: env,
   entry: {
-    // index: path.resolve(__dirname, `./src/index.js`),
     style: path.resolve(__dirname, `./src/style/index.js`)
-  },
-  output: {
-    publicPath,
   },
   devServer: {
     contentBase: path.join(__dirname, "dist"),
@@ -36,19 +27,12 @@ module.exports = {
         exclude: /node_modules/,
         use: {
           loader: require.resolve('babel-loader'),
-          // options: {
-          //   rootMode: 'upward',
-          // },
         },
       },
       {
         test: /\.s[ac]ss$/i,
         use: [
-          // fallback to style-loader in development
-          // !isProduction
-          //   ? { loader: 'style-loader', options: { injectType: 'styleTag' } } // injeta
-          //   : MiniCssExtractPlugin.loader, // extrai pra arquivo
-            MiniCssExtractPlugin.loader,  
+          MiniCssExtractPlugin.loader,  
           'css-loader',
           {
             loader: 'sass-loader',
@@ -73,15 +57,19 @@ module.exports = {
     new ModuleFederationPlugin({
       name: 'minimal_ui',
       library: { type: "var", name: "minimal_ui" },
-      // library: { type: "global", name: 'lib_demo' },
-      // library: { type: "system" },
       filename: "remoteEntry.js",
       exposes: {
         './components': "./src/components",
         './utils': "./src/utils",
-        // './style': "./src/style",
       },
-      shared: ["preact", "idb"]
+      shared: { 
+        preact: {
+          eager: true,
+        },
+        idb: {
+          eager: true,
+        },
+      }
     }),    
     new MiniCssExtractPlugin({
       // Options similar to the same options in webpackOptions.output
@@ -89,14 +77,7 @@ module.exports = {
       filename: '[name].css',
       chunkFilename: '[id].css',
       disable: !isProduction,
-    }),
-    // new HtmlWebpackPlugin({
-    //   // minify: isProduction,
-    //   // injetar os bundles automaticamente
-    //   inject: true,
-    //   minify: false,
-    //   template: 'index.html',
-    // }),
+    })
   ],
   optimization: {
     minimize: isProduction,
@@ -108,9 +89,6 @@ module.exports = {
         safari10: false,
       },
     })],
-    // splitChunks: {
-    //   chunks: 'all',
-    // },
   },
   ...addOptions,
 }
